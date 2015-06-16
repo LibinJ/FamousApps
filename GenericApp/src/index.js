0823c18dd3fa3095ee79a56c3c1a14e061dab706
Yahang Zhang <yahangz@seas.upenn.edu>
2015-06-16 10:57:51 -0700

    update architecture

--- GenericApp/src/index.js
+++ GenericApp/src/index.js
@@ -1,5 +1,5 @@
-var HamburgerMenu = require('./hamburgerMenu/HamburgerMenu');
-var Page = require('./page/Page');
+var HamburgerMenu = require('./Views/hamburgerMenu/HamburgerMenu');
+// var Page = require('./page/Page');
 var famous = require('famous');
 var FamousEngine = famous.core.FamousEngine;
 
