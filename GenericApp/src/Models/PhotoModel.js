03a9ee778b1a0aca3e9505c086767e7a09a71195
Albert Chen <chenalbert@mail.com>
2015-06-25 14:28:20 -0700

    fff

--- GenericApp/src/Models/PhotoModel.js
+++ GenericApp/src/Models/PhotoModel.js
@@ -3,7 +3,8 @@ var PhotoModel = Backbone.Model.extend({
 	defaults: {
 		imageUrl: 'http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg',
 		title: 'cute cat',
-        type: 'Cat'
+        type: 'Cat',
+        subtitle: 'Daniel is very very hansome'
 	}
 });
 
